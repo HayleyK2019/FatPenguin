dc04e4cd13282b69e2e6de38f357b703eafe6652
hayley <hayleykinnear93@gmail.com>
2020-01-27 11:45:02 +0000

    changed settings

--- fatpenguin/fatpenguin/settings.py
+++ fatpenguin/fatpenguin/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  '/Users/Hayley/Desktop/Profile/myprofile/assets'
+STATIC_ROOT =  '/Users/Hayley/Desktop/fatpenguin/fatpenguin/assets'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
